1aef76bf1eedacdaef96103bb606678d98e8ad55
Jake Meyer <jacob.robert.meyer@gmail.com>
2014-02-11 17:39:57 -0700

    end of day Tuesday

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-var scene, camera, renderer, controls;
+
 
 var init = function(){
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	camera = new THREE.PerspectiveCamera( 75, 400 / 400, 0.1, 1000 );
 	camera.position.z = 50;
 	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( 600, 400 );
+	renderer.setSize( 400, 400 );
 	renderer.setClearColor(0xFFFFFF);
 	controls = new THREE.OrbitControls(camera, renderer.domElement);
+	pointLight = new THREE.PointLight(0xFFFFFF); // Set the color of the light source (white).
+    pointLight.position.set(50, 50, 250); // Position the light source at (x, y, z).
+    scene.add(pointLight);
 	$('#view').append( renderer.domElement );
+	return {scene:scene,camera:camera,renderer:renderer,pointLight:pointLight}	
 }
 
 var render = function() {
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 	controls.update();
+}
+
+var clearScene = function(scene) {
+    var objsToRemove = _.rest(scene.children, 1);
+    _.each(objsToRemove, function( object ) {
+          scene.remove(object);
+    });
 }
 
 var toPolar = function(x,y,z){
@@ -79,6 +90,46 @@ var calcMiddle = function(startVector,endVector) {
 	return middlePosition
 }
 
+var makeSpheres = function(dataMatrix) {
+	_.map(dataMatrix,function(node,ind){
+		newSphere(node,ind);
+	});
+}
+
+var newSphere = function(node,ind) {
+	var geometry = new THREE.SphereGeometry(1,25,25);
+	var material = new THREE.MeshPhongMaterial( { color: 0x758EF4} );
+	var sphere = new THREE.Mesh(geometry,material);
+	var sphereCoords = calcSphereCoords(node,ind);
+	sphere.position.set(sphereCoords.xCoord,sphereCoords.yCoord,sphereCoords.zCoord);
+	scene.add(sphere);
+	node.sphere = sphere;
+}
+
+var calcSphereCoords = function(node,ind) {
+	var quadrants = [{x:1,y:1,z:1},{x:1,y:-1,z:1},{x:1,y:1,z:-1},{x:1,y:-1,z:-1},{x:-1,y:1,z:1},{x:-1,y:-1,z:-1},{x:-1,y:1,z:-1},{x:-1,y:-1,z:1}]
+	var xOffset = quadrants[ind%8].x
+	var yOffset = quadrants[ind%8].y;
+	var zOffset = quadrants[ind%8].z;
+	var xCoord = node.centrality ? xOffset / node.centrality : 0;
+	var yCoord = node.centrality ? yOffset / node.centrality : 0;
+	var zCoord = node.centrality ? zOffset / node.centrality : 0;
+	return { xCoord:xCoord, yCoord:yCoord, zCoord:zCoord }
+}
+
+var makeEdges = function(dataMatrix) {
+	_.map(dataMatrix,function(node){
+		for (var i=0; i<node.connectionArray.length; i++) {
+			if (node.connectionArray[i]) {
+				var edge = mapLine(node.sphere,dataMatrix[i].sphere);
+				node.edges.push(edge);
+				dataMatrix[i].edges.push(edge);
+			}
+		}
+	})
+	return dataMatrix;
+}
+
 var mapLine = function(node1,node2) {
 	var startVector = node1.position;
 	var endVector = node2.position;
@@ -97,86 +148,175 @@ var mapLine = function(node1,node2) {
 	}
 	var line = new THREE.Line(lineGeometry,lineMaterial);
 	scene.add(line);
+	return line;
 }
 
-var controller = init();
-
-var geometry = new THREE.SphereGeometry(1,25,25);
-var material = new THREE.MeshPhongMaterial( { color: 0xEA76F7} );
-var sphere = new THREE.Mesh(geometry,new THREE.MeshPhongMaterial({color:0xFF0000}));
-// scene.add(sphere);
-
-
-var initData = [];
-for (var i=0; i<40; i++) {
-	initData.push(Faker.Helpers.createCard());
-}
-
-_.map(initData,function(person){
-	person.friends = [];
-	var numFriends = Math.floor(Math.random()*initData.length/4+1);
-	for (var i=0; i<numFriends; i++) {
-		person.friends.push(Math.floor(Math.random()*initData.length));
+var makeFriendData = function(dataSize) {
+	var dataMatrix = [];
+	for (var i=0; i<dataSize; i++) {
+		dataMatrix.push(Faker.Helpers.createCard());
 	}
-});
-
-
-var newData = [];
-_.map(initData,function(person,ind) {
-	var personData = {};
-	personData.name = person.name;
-	personData.connectionArray = [];
-	for(var i=0; i<initData.length; i++) {
-		personData.connectionArray.push(0);
-	}
-	for(var j=0; j<person.friends.length; j++) {
-		personData.connectionArray[person.friends[j]] = 1;
-	}
-	personData.friends = person.friends;
-	personData.words = [];
-	for (var i=0; i<3; i++){
-		personData.words.push(person.posts[i].sentence);  
-	}
-	newData.push(personData);
-})
-
-var centralities = calcCentrality(newData,20);
-_.map(centralities,function(num,ind){
-	newData[ind].centrality = num.toFixed(4);
-})
-
-newData = _.sortBy(newData,function(node){
-	return 1/Number(node.centrality);
-})
-
-_.map(newData,function(person,ind){
-	var sphere = new THREE.Mesh(geometry,material);
-	var xOffset = (Math.random()>.5) ? 1 : -1;
-	var yOffset = (Math.random()>.5) ? 1 : -1;
-	var zOffset = (Math.random()>.5) ? 1 : -1;
-	var xCoord = person.centrality ? xOffset / person.centrality : 0;
-	var yCoord = person.centrality ? yOffset / person.centrality : 0;
-	var zCoord = person.centrality ? zOffset / person.centrality : 0;
-	sphere.position.set(xCoord,yCoord,zCoord);
-	scene.add(sphere);
-	person.sphere = sphere;
-});
-
-_.map(newData,function(node){
-	for (var i=0; i<node.connectionArray.length; i++) {
-		if (node.connectionArray[i]) {
-			mapLine(node.sphere,newData[i].sphere)
+	_.map(dataMatrix,function(node,ind){
+		node.id = ind;
+		node.on = false;
+		node.friends = [];
+		var numFriends = Math.floor(Math.random()*dataMatrix.length/4+1);
+		for (var i=0; i<numFriends; i++) {
+			var newFriend = Math.floor(Math.random()*dataMatrix.length)
+			if (newFriend !== ind) {
+				node.friends.push(dataMatrix[newFriend].name);
+			}
 		}
-	}
+	});
+
+	_.map(dataMatrix,function(node,ind) {
+		node.connectionArray = [];
+		for(var i=0; i<dataMatrix.length; i++) {
+			node.connectionArray.push(0);
+		}
+		for(var j=0; j<node.friends.length; j++) {
+			var friendInd = _.find(dataMatrix,function(otherNode){
+				return otherNode.name === node.friends[j];
+			})
+			node.connectionArray[friendInd.id] = 1;
+		}
+		node.edges=[];
+		
+	})
+	return dataMatrix
+}
+
+var getConnectedNodes = function(node, dataMatrix) {
+	var connectedNodes = [];
+	_.map(node.friends,function(friend){
+		var friendObj = _.find(dataMatrix,function(otherNode){
+			return friend === otherNode.name;
+		})
+		connectedNodes.push(friendObj);
+	})
+	return connectedNodes;
+}
+
+var makeGraph = function(dataMatrix, scene) {
+	
+	// var sphere = new THREE.Mesh(geometry,new THREE.MeshPhongMaterial({color:0xFF0000}));
+	// scene.add(sphere);
+
+	var centralities = calcCentrality(dataMatrix,20);
+	_.map(centralities,function(num,ind){
+		dataMatrix[ind].centrality = num.toFixed(4);
+	})
+	makeSpheres(dataMatrix, scene);
+	makeEdges(dataMatrix, scene);
+	makeNodeLabels(dataMatrix, scene);
+	initData = _.sortBy(dataMatrix,function(node){
+		return 1/Number(node.centrality);
+	})
+	return initData
+}
+
+var makeNodeLabel = function(node){
+	var canvas1 = document.createElement('canvas');
+	canvas1.width = 100;
+	canvas1.height = 50;
+	var context1 = canvas1.getContext('2d');
+	context1.font = "Bold 10px Arial";
+	context1.fillStyle = "rgba(255,0,0,0.95)";
+	context1.fillText(node.name, 0, 20);
+
+	// canvas contents will be used for a texture
+	var texture1 = new THREE.Texture(canvas1) 
+	texture1.needsUpdate = true;
+	  
+	var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
+	material1.transparent = true;
+	var position = node.sphere.position;
+	var mesh1 = new THREE.Mesh(
+	    new THREE.PlaneGeometry(20, 10),
+	    material1
+	  );
+	mesh1.position.set(position.x,position.y,position.z);
+	node.label = mesh1;
+}
+
+var makeNodeLabels = function(dataMatrix) {
+	_.map(dataMatrix,function(node){
+		makeNodeLabel(node)
+	})
+}
+
+
+
+$(function(){
+	var scene, camera, renderer, controls, pointLight;
+	var threeObj = init(scene, camera, renderer, controls, pointLight);
+	var initData = makeFriendData(20);
+	initData = makeGraph(initData, threeObj.scene);
+	render();
+
+	var tableTemplate = Handlebars.compile($('#tableTemplate').text());
+	$('#tableBody').append($(tableTemplate(initData)));
+
+	$(document).on('click','#newData',function(e){
+		e.preventDefault();
+		clearScene(threeObj.scene);
+		initData = makeFriendData(20);
+		initData = makeGraph(initData, threeObj.scene);
+		render();
+		$('#tableBody').html('');
+		$('#tableBody').append($(tableTemplate(initData)));
+	})
+
+	$(document).on('click','.table-node-row',function(){
+		
+		var id = parseInt($(this).attr('data-id'));
+		var node = _.find(initData,function(node){
+			return node.id === id;
+		})
+		var edges = node.edges;
+		console.log(threeObj.scene);
+		if (!node.on) {
+			node.sphere.material.color.setHex(0xEA76F7);
+			_.map(edges,function(edge){
+				edge.material.color.setHex(0xEA76F7);
+			});
+			var connectedNodes = getConnectedNodes(node,initData);
+			_.map(connectedNodes,function(node){
+				node.sphere.material.color.setHex(0xF4A1AC);
+				threeObj.scene.add(node.label);
+			})
+			threeObj.scene.add(node.label);
+			node.on = true;
+			$(this).css('background-color','#EA76F7');
+		} else {
+			node.sphere.material.color.setHex(0x758EF4);
+			_.map(node.edges,function(edge){
+				edge.material.color.setHex(0x999999);
+			});
+			var connectedNodes = getConnectedNodes(node,initData);
+			_.map(connectedNodes,function(node){
+				node.sphere.material.color.setHex(0x758EF4);
+				threeObj.scene.remove(node.label);
+			})
+			threeObj.scene.remove(node.label);
+			node.on = false;
+			$(this).css('background-color','');
+		}
+	})
+
+	$('.scrollable').on('DOMMouseScroll mousewheel', function(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        ev.returnValue = false;
+        return false;
+	});
+
+	$(document).on('click', '#showTable',function(){
+		$('table').fadeToggle();
+		setTimeout(function(){
+			$('#showTable').text(($('table').css('display') === 'none') ? "Show table" : "Hide table");
+		}, 500)
+		
+	})
+
 })
-
- var pointLight = new THREE.PointLight(0xFFFFFF); // Set the color of the light source (white).
-    pointLight.position.set(50, 50, 250); // Position the light source at (x, y, z).
-    scene.add(pointLight);
-
-render();
-
-
-var tableTemplate = Handlebars.compile($('#tableTemplate').text());
-
-$('#tableBody').append($(tableTemplate(newData)));
